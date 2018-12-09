245f0831ee54f5e44a727658344af18f07517623
Pratik Parmar <prateek18597@gmail.com>
2018-12-09 13:33:21 +0530

    Backprop of FC layer

--- CNN.py
+++ CNN.py
@@ -7,6 +7,10 @@ import scipy.io as sio
 from scipy.stats import pearsonr
 import pandas as pd
 
+def sigmoid(x):#Sigmoid activation function
+  return 1 / (1 + math.exp(-x))
+
+
 def conv(X_matrix,filter):#Does convolution for a matrix and filter
 	X=np.zeros([len(X_matrix)-len(filter)+1,len(X_matrix[0])-len(filter)+1])
 	for j in range(0,len(X_matrix)-len(filter)+1,1):
@@ -14,23 +18,37 @@ def conv(X_matrix,filter):#Does convolution for a matrix and filter
 			for m in range(0,len(filter),1):
 				for n in range(0,len(filter[0]),1):
 					X[j,k]+=X_matrix[j+m][k+n]*filter[m][n];
+	
+	for j in range(0,len(X_matrix)-len(filter)+1,1):#Applying ReLU Activation function
+		for k in range(0,len(X_matrix[0])-len(filter[0])+1,1):
+				if X[i][j]<0:
+					X[i][j]=0;
+
 	return X;
 
 def pool(X_matrix,filter,stride):#Does max pooling for X_matrix
 	n_h=math.ceil((len(X_matrix)-len(filter))/stride)+1
 	n_w=math.ceil((len(X_matrix[0])-len(filter))/stride)+1
 	X=np.zeros([n_h,n_w]);
+	index=np.zeros([n_h,n_w]);
 	for j in range(0,n_h,1):
 		for k in range(0,n_w,1):
 			for m in range(0,len(filter),1):
 				for n in range(0,len(filter[0]),1):
 					if X[j,k]<X_matrix[j+m][k+n]:
 						X[j,k]=X_matrix[j+m][k+n];	
-	return X;
+						index[j,k]=(j+m,k+n)
+	return X,index;
 
-def fully_connected(X_matrix):
+
+def fully_connected(X_matrix,filter):
 	X = np.asarray(X_matrix).reshape(-1)
-	return X;
+	output=0;
+	for i in range(0,len(X)):
+		output+=X[i]*filter[i];
+	sig_output=sigmoid(output)
+	return sig_output;
+
 
 
 def get_feature(img1,img2,y,img1_name,img2_name,value):
@@ -178,66 +196,80 @@ def main():
 	train=math.floor(len(ref_images_name)*0.9)#10 fold cross validation
 	print(train)
 	counter=0;
-
+	mat_contents = sio.loadmat('dmos.mat')
+	dmos=mat_contents["dmos"];
+	for i in range(0,len(ref_images_name)):
+		im=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[i]),0)
+		images[ref_images_name[i]]=im;
+		
 # for fold in range(1,10):#Doing 10 fold cross validation
 	train_images={}
 	test_images={}
 	
 	for i in range(0,counter):
-		img=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[i]),0)
-		train_images[ref_images_name[i]]=img
+		# img=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[i]),0)
+		train_images[ref_images_name[i]]=images[ref_images_name[i]]
 
 	if counter==27:
 		for i in range(0,2):
 			img=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[counter]),0)
-			test_images[ref_images_name[counter]]=img
+			test_images[ref_images_name[counter]]=images[ref_images_name[counter]]
 			counter+=1
 	else:
 		for i in range(0,3):
 			img=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[counter]),0)
-			test_images[ref_images_name[counter]]=img
+			test_images[ref_images_name[counter]]=images[ref_images_name[counter]]
 			counter+=1
 
 	for i in range(counter,len(ref_images_name)):
 		img=cv2.imread("./Live Database/databaserelease2/refimgs/"+str(ref_images_name[i]),0)
-		train_images[ref_images_name[i]]=img
+		train_images[ref_images_name[i]]=images[ref_images_name[i]]
 	
 	
-	mat_contents = sio.loadmat('dmos.mat')
-	dmos=mat_contents["dmos"];
 	X,Y,corr_img,dist_img=get_training_images(train_images,dmos[0])
 	print(X[0],Y[0])
 
 	max_features=0;
+	filter=[[1,1,1],[1,1,1],[1,1,1]]#Filter for 1st forward pass, It will get modified by Stochastic gradient
+	filter_pool=[[0,0],[0,0]]
+	filter_cc=[1]*105
+	stride=2
+	learning_rate=0.1
 	for i in range(0,len(X)):
-		if len(X[i])>max_features:
-			max_features=len(X[i])
-
-	F = np.zeros((len(X),max_features));
-
-	for i in range(0,len(X)):
-		for j in range(0,len(X[i])):
-			F[i][j]=X[i][j]
 		
-	svr_rbf = SVR(kernel='rbf', C=1000, gamma=0.1)
-	fi=svr_rbf.fit(F,Y);
-	y_rbf=fi.predict(F)
+		#Forward Propagation
 
-	print("PLCC for Training Data:")
-	print(pearsonr(y_rbf,Y))
+		relu_cnn_output=conv(X[i],filter);
+		maxpool_output,index_pool=pool(relu_cnn_output,filter_pool,stride)
+		
+		input_full= np.asarray(maxpool_output).reshape(-1)
+		output=fully_connected(maxpool_output,filter_cc)
+		y=Y[0]/100.0
+		
+		#Backward Propagation for parameters of fully connected layer
+		for j in range(0,105,1):
+			filter_cc[j]=filter_cc[j]+learning_rate*(Y[0]-output)*output*(1-output)*input_full[j];
+
+		#Backward Propagation for parameter of Covolutional Layer
+		for j in range(0,3,1):
+			for k in range(0,3,1):
+				#Write Backprop equation.
+
+	# print("PLCC for Training Data:")
+	# print(pearsonr(y_rbf,Y))
 
 	x_test,y_test,corr_img_test,dist_img_test=get_training_images(test_images,dmos[0]);
 
-	T = np.zeros((len(x_test),min_features));
+	# T = np.zeros((len(x_test),min_features));
 
-	for i in range(0,len(x_test)):
-		for j in range(0,len(x_test[i])):
-			T[i][j]=x_test[i][j]
+	# for i in range(0,len(x_test)):
+	# 	for j in range(0,len(x_test[i])):
+	# 		T[i][j]=x_test[i][j]
 	
-	y_rbf_test=fi.predict(T)
+	# y_rbf_test=fi.predict(T)
 
-	print("PLCC for Testing Data:")
-	print(pearsonr(y_test,y_rbf_test))
+	# print("PLCC for Testing Data:")
+	# print(pearsonr(y_test,y_rbf_test))
 
 # main();
 print(fully_connected(conv([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],[[0,1,0],[0,1,0],[0,1,0]])));
